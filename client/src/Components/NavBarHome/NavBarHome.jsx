22401a718f4adc6b7fd629614065e109105e5cf8
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-03-03 01:51:01 +0530

    home cominjkbib

--- client/src/Components/NavBarHome/NavBarHome.jsx
+++ client/src/Components/NavBarHome/NavBarHome.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./NavBarHome.css"
 import { Link, useNavigate } from 'react-router-dom'
-import { Link, useNavigate } from 'react-router-dom'
 
 const NavBarHome = () => {
     const navigate = useNavigate();
@@ -14,7 +13,6 @@ const NavBarHome = () => {
                         <p title="StudApp">StudApp</p>
                     </div>
                 </Link>
-                <div className='menu-icon' onClick={()=>navigate("/profile")}>
                 <div className='menu-icon' onClick={()=>navigate("/profile")}>
                     <i className='bx bx-user-circle' style={{fontSize:"2rem",marginTop:"8px"}}></i>
                 </div>
