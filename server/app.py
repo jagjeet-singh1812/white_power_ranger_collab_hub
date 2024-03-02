87b6d3522d741713d577e09bae690b51b1158f76
Sai Kaushik Sadu <91894475+SaiKaushikSadu@users.noreply.github.com>
2024-03-03 02:55:25 +0530

    app wroing

--- server/app.py
+++ server/app.py
@@ -1,27 +1,31 @@
-from flask import Flask,request,jsonify
-from flask_cors import CORS
-from youtube_transcript_api import YouTubeTranscriptApi as yta
-import re
+from flask import Flask, request
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
-def index():
-    video_id="97V4BhuN2X8"
-    data= yta.get_transcript(video_id)
-    transcript= ""
-    for value in data:
-        for key,val in value.items():
-            if key== "text":
-                transcript += val
-    print(transcript)
-    return jsonify({
-            'text':transcript
-        })
+@app.get('/summary')
+def summary_api():
+    # url = request.args.get('url', 'Big_aFLmekI')
+    # video_id = url.split('=')[1]
+    video_id="Big_aFLmekI"
+    summary = get_summary(get_transcript(video_id))
+    return summary, 200
 
+def get_transcript(video_id):
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+    transcript = ' '.join([d['text'] for d in transcript_list])
+    return transcript
 
-
-CORS(app)
+def get_summary(transcript):
+    summariser = pipeline('summarization',model="facebook/bart-large-cnn")
+    summary = ''
+    for i in range(0, (len(transcript)//1000)+1):
+        summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
+        summary = summary + summary_text + ' '
+    return summary
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3001)
+    app.run(port=5000)
+    
